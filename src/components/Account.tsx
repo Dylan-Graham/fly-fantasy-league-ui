59ad79e216bddcc445e5d43e207b1aae8a202e7a
Dylan Graham <dylan@explore-ai.net>
2022-07-30 10:15:20 +0200

    Account page

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import "./Account.css";
 
 export const Account = () => {
   return (
     <div>
-      <p>Name: Alice Bob</p>
-      <p>Email: a.bob@gmail.com</p>
-      <Button variant="contained">Deactivate account</Button>
+      <div className="account">
+        <p className="text">Name: Alice Bob</p>
+        <p className="text">Email: a.bob@gmail.com</p>
+      </div>
+      <Button variant="contained" color="error" className="deactivate-button">
+        Deactivate account
+      </Button>
     </div>
   );
 };
