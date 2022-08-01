851e76b6710602497aac4c26a2edbebfd95114ee
Dylan Graham <dylan@explore-ai.net>
2022-08-01 23:41:11 +0200

    - Add athlete logic (1st step done, 2nd style, 3rd add extra data) - Picks works - Background

--- src/components/Add-Athlete.tsx
+++ src/components/Add-Athlete.tsx
@@ -2,14 +2,70 @@ import React from "react";
 import "./Add-Athlete.css";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
 
-export const AddAthlete = () => {
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+interface athlete {
+  id: string;
+  name: string;
+}
+
+export const AddAthlete = (props: any) => {
+  const [open, setOpen] = React.useState(false);
+  const [athleteName, setAthleteName] = React.useState("Add Athlete");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (athleteName: string) => {
+    setAthleteName(athleteName);
+    setOpen(false);
+  };
+
+  const athletes = props.athletes;
+
   return (
     <div className="add-athlete-button">
-      <Button variant="outlined" size="small">
+      <Button variant="outlined" size="small" onClick={handleClickOpen}>
         <AddIcon fontSize="small" />
       </Button>
-      <p className="add-athlete-text">Add athlete</p>
+      <p className="add-athlete-text">{athleteName}</p>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        onClose={handleClose}
+        aria-labelledby="pick-dialog-title"
+        aria-describedby="pick-dialog-description"
+      >
+        <DialogTitle id="pick-dialog-title">{"Pick an Athlete"}</DialogTitle>
+        <DialogContent>
+          <div className="athletes">
+            {athletes.map((athlete: athlete) => (
+              <Button
+                key={athlete.id}
+                variant="outlined"
+                className="athleteButton"
+                onClick={() => handleClose(athlete.name)}
+              >
+                {athlete.name}
+              </Button>
+            ))}
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- src/components/Picks.tsx
+++ src/components/Picks.tsx
@@ -2,6 +2,34 @@ import React from "react";
 import "./Picks.css";
 import { AddAthlete } from "./Add-Athlete";
 
+export interface athlete {
+  id: number;
+  name: string;
+}
+
+const tierA: athlete[] = [
+  { id: 1, name: "Andrea Principe" },
+  { id: 2, name: "Giel Vlugte" },
+  { id: 3, name: "Luca Ceruti" },
+  { id: 4, name: "Lorenzo Casati" },
+];
+
+const tierB = [
+  { id: 1, name: "Cohan Van Djik" },
+  { id: 2, name: "Stig Hoefnagel" },
+  { id: 3, name: "Milan Lukic" },
+  { id: 4, name: "Ross-Dillon Player" },
+  { id: 5, name: "Jett Bradshaw" },
+];
+
+const tierC = [
+  { id: 1, name: "Robby James" },
+  { id: 2, name: "Julian Zens" },
+  { id: 3, name: "Dylan Graham" },
+  { id: 4, name: "Aron Rosslee" },
+  { id: 5, name: "Rob Kritzinger" },
+];
+
 export const Picks = () => {
   return (
     <div className="picks">
@@ -13,22 +41,22 @@ export const Picks = () => {
 
       <div>
         <h4>Tier A (top 6)</h4>
-        <AddAthlete />
-        <AddAthlete />
+        <AddAthlete athletes={tierA} />
+        <AddAthlete athletes={tierA} />
       </div>
 
       <div>
         <h4>Tier B (top 12)</h4>
-        <AddAthlete />
-        <AddAthlete />
-        <AddAthlete />
-        <AddAthlete />
+        <AddAthlete athletes={tierB} />
+        <AddAthlete athletes={tierB} />
+        <AddAthlete athletes={tierB} />
+        <AddAthlete athletes={tierB} />
       </div>
 
       <div>
         <h4>Tier C (Wild Picks)</h4>
-        <AddAthlete />
-        <AddAthlete />
+        <AddAthlete athletes={tierC} />
+        <AddAthlete athletes={tierC} />
       </div>
     </div>
   );
